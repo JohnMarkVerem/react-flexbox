9d62f6fd0c09732cdf016c694b015d5a7ea59d9e
JohnMarkVerem <verembobo7070@gmail.com>
2024-04-03 15:22:28 +0100

    my react assignment

---
+++ src/Flexbox.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Flexbox.css";
+function Flexbox() {
+    return (
+        <div className="cube">
+            <div className="grid_container">
+                <h2 className="fanta">fanta</h2>
+                <h2 className="coke">coke</h2>
+                <h2 className="sprite">sprite</h2>
+                <h2 className="chapman">chapman</h2>
+            
+                <h2 className="banana">banana</h2>
+                <h2 className="apple">apple</h2>
+                <h2 className="mango">mango</h2>
+                <h2 className="orange">orange</h2>
+
+                <h2 className="plastic">plastic</h2>
+                <h2 className="wood">wood</h2>
+                <h2 className="cement">cement</h2>
+                <h2 className="glass">glass</h2>
+            
+                <h2 className="chicken">chicken</h2>
+                <h2 className="beef">beef</h2>
+                <h2 className="pork">pork</h2>
+                <h2 className="fish">fish</h2>
+            </div>
+        </div>
+
+    
+    );
+}
+export default Flexbox;
